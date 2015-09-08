ace270d8cb7ca3cca70cd67d87d4fd39185c3a65
garimakhulbe <garimakhulbe@users.noreply.github.com>
2015-09-08 11:52:22 -0700

    Create autoscaleConstants.js

---
+++ src/autoscaleConstants.js
@@ -0,0 +1,11 @@
+exports = module.exports;
+
+var autoscaleConstants = {
+    TIMEOUT: 120,
+    STORAGE_READ_INTERVAL: 10,
+    STORAGE_WRITE_INTERVAL: 20,
+    CHECK_STATUS_INTERVAL: 60
+
+};
+
+module.exports = autoscaleConstants;
